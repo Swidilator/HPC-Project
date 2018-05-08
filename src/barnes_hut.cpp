9cf902d4058b9e14621ea2f0c3daa06e8bcb4956
Swidilator <kyleweiher@gmail.com>
2018-05-08 12:58:21 +0200

    Finished force calculation

--- src/barnes_hut.cpp
+++ src/barnes_hut.cpp
@@ -2,28 +2,34 @@
 
 
 
-void simulate(array<Body, NUM_BODIES>* bodies, Octree *octree)
+void simulateStep(array<Body, NUM_BODIES>* bodies, Octree *octree)
 {
     Node* headNode = octree->getHeadNode();
     
     for(int i = 0; i < NUM_BODIES; i++)
     {
-
+        
     }
-
-
-}
-
-dim3float calcForce(Body *source, Body *target)
-{
-    float G = 6.674 * (pow((float)10, -11));
-
-    return -G * (source->getMass() * target->getMass())/(pow)
 
 
 }
 
 float calcDistance(Body *source, Body *target)
 {
-    return sqrt(((target->setCentreOfMass() - source->setCentreOfMass())^2)++);
+    dim3float product = ((target->getCentreOfMass() - source->getCentreOfMass())^2);
+    return sqrt(product.x + product.y + product.z);
 }
+
+dim3float calcForce(Body *source, Body *target)
+{
+    float G = 6.674 * (pow((float)10, -11));
+
+    dim3float vector = (target->getCentreOfMass() - source->getCentreOfMass());
+
+    float scalar = -G * (source->getMass() * target->getMass())/(pow(calcDistance(source,target),3));
+
+    return vector * scalar;
+
+
+}
+
